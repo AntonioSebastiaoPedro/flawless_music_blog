c1ad999c45174988ecfc4580ceb1f8ebbc4ba389
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2024-01-11 23:30:59 +0100

    feat: Cria o arquivo de carregamento das views blade

---
+++ opt/load.php
@@ -0,0 +1,11 @@
+<?php
+// opt/load.php
+
+// Inclua as configurações do projeto
+require_once DIRREQ . 'config/config.php';
+
+// Aqui você pode adicionar outras inicializações específicas, se necessário
+// Por exemplo:
+// session_start();
+
+// Fim do arquivo load.php
